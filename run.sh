b998c671ec7b4f31ddf614d25b6bce50a2371a2d
schuerchr <christoph.schuerrle@posteo.de>
2023-10-08 14:49:04 +0200

    Show current migrations when starting the django container

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "> Run all django migrations to update database"
 python ./manage.py migrate
+echo "> Show migrations"
+python ./manage.py showmigrations
 echo "> Run the django server"
 python ./manage.py runserver 0.0.0.0:8000
