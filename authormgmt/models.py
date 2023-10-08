23dec5263ebd17548a529b3186ce62155e24eb31
schuerchr <christoph.schuerrle@posteo.de>
2023-10-08 17:20:54 +0200

    (FAULTY) Added a model to represent authors but there is a typo!
    In this commit a DB model for authors is introduced in authormgmt
    but the dev made a typo in the model name. Whoops

--- authormgmt/models.py
+++ authormgmt/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Athor(models.Model): #NOTE: Here we introduce an intentional typo as error! Should be Author
+    name = models.CharField(max_length=200)
+    birthday = models.DateField()
--- authormgmt/views.py
+++ authormgmt/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Athor
 
 # Create your views here.
 
 def index(request):
-    context = {}
+    authors = list(Athor.objects.order_by("name"))
+    context = {"authors": authors}
     return render(request, "authormgmt/index.html", context)
