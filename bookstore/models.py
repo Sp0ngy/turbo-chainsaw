73a7dafe3e5ce7b86371c06c223a008c9e9b76fc
schuerchr <christoph.schuerrle@posteo.de>
2023-10-08 17:38:58 +0200

    Bookstore now displays the Author (via FK Relation)
    Here we extended the Book model with a Author and imported the
    class with the typo.
    We let django generate the required migration containg also the
    typo.
    View was updated to display authors if available.

--- bookstore/models.py
+++ bookstore/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     price = models.IntegerField()
+    author = models.ForeignKey("authormgmt.Athor", on_delete=models.SET_NULL, null=True)
